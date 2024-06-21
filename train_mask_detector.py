9704481fdf5375c9326c8eb74c3a5e9865e9eebc
Govardhana Anirudh <anirudhgovardhana@gmail.com>
2024-06-21 11:00:46 +0530

    added-new-project-files

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,80 +1,86 @@
-# import the necessary packages
+# Import necessary packages
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
+# Initialize constants
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+# Path to dataset directory and categories
+DIRECTORY = r"C:\Users\aniru\Desktop\Face-Mask-Detection2\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
-print("[INFO] loading images...")
+# Load images and labels
+print("[INFO] Loading images...")
 
 data = []
 labels = []
 
+# Load and preprocess images
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
+        img_path = os.path.join(path, img)
+        image = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = preprocess_input(image)
 
-    	data.append(image)
-    	labels.append(category)
+        data.append(image)
+        labels.append(category)
 
-# perform one-hot encoding on the labels
+# Convert lists to numpy arrays
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+# Perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
-data = np.array(data, dtype="float32")
-labels = np.array(labels)
+# Split data into training and testing sets
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
 
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+# Define data augmentation function
+def augment(image, label):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_flip_up_down(image)
+    image = tf.image.random_brightness(image, max_delta=0.1)
+    image = tf.image.random_contrast(image, lower=0.9, upper=1.1)
+    image = tf.image.random_saturation(image, lower=0.9, upper=1.1)
+    image = tf.image.random_hue(image, max_delta=0.1)
+    image = tf.clip_by_value(image, 0, 1)
+    return image, label
 
-# construct the training image generator for data augmentation
-aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+# Create TensorFlow Dataset objects
+train_data = tf.data.Dataset.from_tensor_slices((trainX, trainY))
+train_data = (
+    train_data
+    .shuffle(buffer_size=len(trainX))
+    .map(augment, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    .batch(BS)
+    .repeat()
+    .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+)
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
-baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+val_data = tf.data.Dataset.from_tensor_slices((testX, testY)).batch(BS)
 
-# construct the head of the model that will be placed on top of the
-# the base model
+# Load MobileNetV2 base model
+baseModel = MobileNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(224, 224, 3)))
+
+# Construct head of the model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -82,47 +88,40 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# Combine base and head models
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+# Freeze base layers
 for layer in baseModel.layers:
-	layer.trainable = False
+    layer.trainable = False
 
-# compile our model
-print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+# Compile the model
+opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-# train the head of the network
-print("[INFO] training head...")
+# Train the model
+print("[INFO] Training head...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    train_data,
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=val_data,
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS
+)
 
-# make predictions on the testing set
-print("[INFO] evaluating network...")
+# Evaluate the model
+print("[INFO] Evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
-
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
-print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
+# Classification report
+print(classification_report(testY.argmax(axis=1), predIdxs, target_names=lb.classes_))
 
-# serialize the model to disk
-print("[INFO] saving mask detector model...")
-model.save("mask_detector.model", save_format="h5")
+# Save the model
+print("[INFO] Saving mask detector model...")
+model.save("mask_detector.h5")
 
-# plot the training loss and accuracy
+# Plot training history
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
